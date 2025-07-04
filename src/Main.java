da863b8e08e7da6bc8e372fb750f9e91c0fa012f
sirconscious <mehdielbakouri0@gmail.com>
2025-07-04 15:08:16 +0100

    adding columns

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@
 public class Main {
     public static void main(String[] args) {
         DB db = new DB("myDB") ;
-        Table usersTable =    db.createTable("users");
-        Table postsTable = db.table("posts");
-        System.out.println(postsTable.getPath());
+         //Table usersTable =    db.createTable("users");
+        Table blogs = db.table("blogs") ;
+        blogs.defineColumns(new String[]{"id", "title" , "body"});
     }
 }
--- src/Table.java
+++ src/Table.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 
 public class Table {
     public String dbPath ;
@@ -20,9 +22,30 @@ public class Table {
         }else {
             try {
                 file.createNewFile();
+
             }catch (IOException e){
                 System.out.println(e.getMessage());
             }
+        }
+    }
+    public void deletTable(){
+        File file = new File(dbPath + "/" + tableName + ".csv");
+        if (!file.exists()){
+            System.out.println("the table " +tableName + " does not exist");
+        }else if(file.delete()){
+            System.out.println("the table " + tableName + " is deleted successfully");
+        }
+    }
+
+    public void defineColumns(String[] columns){
+
+        try (FileWriter file = new FileWriter(dbPath + "/" + tableName + ".csv") ;){
+            for (int i = 0 ; i < columns.length ; i++){
+                file.append(columns[i])
+                        .append(i < columns.length - 1 ? "," : "\n");            }
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
         }
     }
 }
